c219f31ad0dfd6763e2e89715611fe47e0e7afb1
RC <recomi40@gmail.com>
2018-05-15 08:32:57 -0400

    test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     // For each articles in the db
     for (var i = 0; i < data.length; i++) {
       if (data.indexOf(data[i].title) >= -1) {
-        // Display the apropos information on the page
+        // Display the pages information on the page
         $("#article").append("<div class='card' style='width: 22rem;' id='" + data[i]._id + "'>" +
           "<img class='card-img-top'src='" + data[i].image + "' alt='Card image cap'>" +
           "<div class='card-body'><h5 class='card-title'>" + data[i].title + "</h5>" +
